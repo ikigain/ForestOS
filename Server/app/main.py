5f2e43d609baaaf14757183ed1918ebbdb09ba39
ikigain <willenz@gmail.com>
2025-12-01 10:14:34 +0800

    Fix main.py settings attributes for deployment

--- Server/app/main.py
+++ Server/app/main.py
@@ -4,14 +4,14 @@ from app.core.config import settings
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
-    openapi_url=f"{settings.API_V1_PREFIX}/openapi.json",
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
     debug=settings.DEBUG
 )
 
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.cors_origins,
+    allow_origins=settings.ALLOWED_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
